67b0be1c5900579a66898d9c174eb230efe0af6f
max kondratiev <max@sbdh.ru>
2019-08-29 16:02:07 +0400

    Редакция 2. Решение 3 задачи.

--- src/Zadanie3/Car.java
+++ src/Zadanie3/Car.java
@@ -1,14 +1,14 @@
 package Zadanie3;
 
 public class Car {
-
-    public String marka, model, color, engine;
+    public String marka, model, color;
     public int distanceTraveled;
     public int masa;
     public boolean dtp;
     public int volumeTank;
+    public Engine engine;
 
-    public Car(String marka, String model, String color, int distanceTraveled, int masa, boolean dtp, int volumeTank, String engine) {
+    public Car(String marka, String model, String color, int distanceTraveled, int masa, boolean dtp, int volumeTank, Engine engine) {
         this.marka = marka;
         this.model = model;
         this.color = color;
@@ -18,11 +18,12 @@ public class Car {
         this.volumeTank = volumeTank;
         this.engine = engine;
     }
+
     public static void main(String[] args) {
-        Car auto1 = new Car("Scoda", "Octavia", "red", 56780, 1450, false, 50,"Dvigatel1");
-        Engine Dvigatel1 = new Engine(false, 1.8, 150, 0.6);
-        double l = auto1.distanceTraveled * Dvigatel1.fuelСonsumption;
+        Engine engine = new Engine(false, 1.8, 150, 0.6);
+        Car auto1 = new Car("Scoda", "Octavia", "red", 56780, 1450, false, 50, engine);
+        double l = auto1.distanceTraveled * engine.fuelСonsumption;
         System.out.println("БИИИИИП");
-        System.out.print("Машина проехала " + auto1.distanceTraveled + " км. и прои этом потратила " + l + " литра(ов) бензина.");
+        System.out.println("Машина потратила " + l + " литра(ов) бензина.");
     }
 }
